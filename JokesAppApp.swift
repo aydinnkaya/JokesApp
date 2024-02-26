b5c818c9cde1549350b1c8d2c1e3b72b136ecf82
Aydın Kaya <Aydinnkaya1@gmail.com>
2024-02-26 18:47:58 +0300

    Initial commit

---
+++ JokesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppApp.swift
+//  JokesApp
+//
+//  Created by Aydın KAYA on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
